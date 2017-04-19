8226b905dcb4db92b8fcd50c48dcadc7c03761b7
Zak Hassan <zak.hassan1010@gmail.com>
2017-04-19 13:08:43 -0400

    Saving pi spark

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def hello():
     return "Pi is roughly %f" % (4.0 * count / n)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
