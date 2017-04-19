f0f28ee77415800bc2ff8fdb31cc198a8f5a646d
Zak Hassan <zak.hassan1010@gmail.com>
2017-04-19 12:42:05 -0400

    Setup web application to listen on port 5000 and run spark job

---
+++ README.md
@@ -0,0 +1,8 @@
+# flask-pyspark-pi-example
+
+To run use the following:
+
+```bash
+
+spark-submit.sh app.py
+```
---
+++ app.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+import os
+from flask import Flask
+import sys
+from random import random
+from operator import add
+
+import pyspark
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession, SQLContext
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    spark = SparkSession.builder.master("local[*]").appName("PythonPi").getOrCreate()
+
+    #TODO: Need to get this value from querystring 'partitions'
+    partitions = 2
+    n = 100000 * partitions
+
+    def f(_):
+        x = random() * 2 - 1
+        y = random() * 2 - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    print("Pi is roughly %f" % (4.0 * count / n))
+
+    spark.stop()
+    return "SparkJobSubmitted"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
