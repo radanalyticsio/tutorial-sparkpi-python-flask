eec016af6faf8762b729aef3f2e1fbc14117ce9d
Michael McCune <msm@redhat.com>
2017-08-15 09:18:34 -0400

    cleanup app.py
    This change removes some unused imports, takes out the print statement
    and adds general pep8 cleanups.

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from __future__ import print_function
-import os
-from flask import Flask
-import sys
 from random import random
 from operator import add
+import os
 
-import pyspark
-from pyspark.context import SparkContext
-from pyspark.sql import SparkSession, SQLContext
+from flask import Flask
+from pyspark.sql import SparkSession
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
+def index():
     spark = SparkSession.builder.appName("PythonPi").getOrCreate()
 
-    #TODO: Need to get this value from querystring 'partitions'
+    # TODO: Need to get this value from querystring 'partitions'
     partitions = 2
     n = 100000 * partitions
 
@@ -24,12 +22,15 @@ def hello():
         y = random() * 2 - 1
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    count = spark.sparkContext.parallelize(
+        range(1, n + 1), partitions).map(f).reduce(add)
+    response = "Pi is roughly {}".format(4.0 * count / n)
 
     spark.stop()
-    return "Pi is roughly %f" % (4.0 * count / n)
+
+    return response
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
